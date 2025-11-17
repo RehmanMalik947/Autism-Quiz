d475808b89f78fc1e52030904fffcc04597d2af6
Ahmad Raza Siddiqi <ahmadrazasiddiqi5@gmail.com>
2025-11-17 21:15:41 +0500

    Add React Navigation and Screens

---
+++ Screens/SignUpScreen.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const SignUpScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignUpScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default SignUpScreen
+
+const styles = StyleSheet.create({})
