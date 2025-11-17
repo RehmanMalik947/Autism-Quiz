6ff9a7c7e8d5ce385c5b79ba9648e9586e458aaf
Ahmad Raza Siddiqi <ahmadrazasiddiqi5@gmail.com>
2025-11-17 23:04:47 +0500

    Add Custom Header

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,14 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Header = () => {
-  return (
-    <View>
-      <Text>Header</Text>
-    </View>
-  )
+interface HeaderProps {
+  title: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  onPressLeft?: () => void;
+  onPressRight?: () => void;
 }
+const Header = ({
+  title,
+  leftIcon,
+  rightIcon,
+  onPressLeft,
+  onPressRight,
+}: HeaderProps) => {
+  return (
+    <SafeAreaView style={styles.Header}>
+      <View style={styles.HeaderLeft}>
+        {leftIcon && (
+          <TouchableOpacity onPress={onPressLeft}>{leftIcon}</TouchableOpacity>
+        )}
+      </View>
+      <Text style={styles.HeaderTitle}>{title}</Text>
+      <View style={styles.HeaderRight}>
+        {rightIcon && (
+          <TouchableOpacity onPress={onPressRight}>
+            {rightIcon}
+          </TouchableOpacity>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Header
+export default Header;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  Header: {
+    height: 54,
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  HeaderTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  HeaderLeft: {
+    // backgroundColor:'red',
+    height: '100%',
+    width: 54,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  HeaderRight: {
+    // backgroundColor:'blue',
+    height: '100%',
+    width: 54,
+  },
+});
--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '../Components/Header'
+import { ArrowLeft } from 'lucide-react-native'
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+  const navigation=useNavigation()
   return (
     <SafeAreaView>
+      <Header title='Autism Quiz' leftIcon={<ArrowLeft/>} onPressLeft={()=>navigation.goBack()} />
       <Text>HomeScreen</Text>
     </SafeAreaView>
   )
